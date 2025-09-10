3557275ca1722f1339e5a13e5c50ab977e505eda
cano <al22760552@ite.edu.mx>
2025-09-10 12:43:22 -0700

    nueva logica mejorada

--- Práctica 3/logic.py
+++ Práctica 3/logic.py
@@ -1,19 +1,29 @@
+from functools import reduce
 
+# --- Funciones de lógica ---
 def suma(a: int, b: int) -> int:
-    """Función que solo depende de sus parámetros"""
     return a + b
 
 def factorizar(a: int) -> int:
-    """Función que calcula el factorial del número ingresado de forma recursiva"""
     if a < 0:
         raise ValueError("El factorial no está definido para números negativos")
     if a == 0 or a == 1:
         return 1
     return a * factorizar(a - 1)
 
-def fibonacci(a):
-    """Función que calcula la sucesión de Fibonacci"""
+def fibonacci(a: int) -> int:
     if a <= 1:
         return a
-    else:
-        return fibonacci(a-1) + fibonacci(a-2)
+    return fibonacci(a-1) + fibonacci(a-2)
+
+def cuadrados(lista):
+    return [n**2 for n in lista]
+
+def filtrar_pares(lista):
+    return list(filter(lambda n: n % 2 == 0, lista))
+
+def suma_total(lista):
+    return reduce(lambda x, y: x + y, lista)
+
+def producto_total(lista):
+    return reduce(lambda x, y: x * y, lista)
