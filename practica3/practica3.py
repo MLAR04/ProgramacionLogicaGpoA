cc7c4dcef5cde835af6c6366ad9ff5e9da31593f
Emely Jimena Martinez Rios <126805426+Emely-Martinez@users.noreply.github.com>
2025-09-11 23:36:08 -0700

    Update practica3.py

--- practica3/practica3.py
+++ practica3/practica3.py
@@ -1,10 +1,7 @@
-# Ejercicio 1: Función suma(a, b)
 
 def suma(a, b):
     """Devuelve la suma de dos números."""
     return a + b
-
-# Ejercicio 2: Factorial recursivo
 
 def factorial(n):
     """Calcula el factorial de un número n de forma recursiva."""
@@ -13,7 +10,6 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-# Ejercicio 3: Fibonacci recursivo
 def fibonacci(n):
     """Calcula el n-ésimo número de Fibonacci de forma recursiva."""
     if n == 0:
@@ -23,26 +19,13 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-# Ejercicio 4: Operaciones con listas
 from functools import reduce
 
 lista = [1,2,3,4,5,6,7,8,9,10]
-
-# 4a) Lista con los cuadrados de cada número usando map
 cuadrados = list(map(lambda x: x**2, lista))
-
-# 4b) Filtrar los números pares usando filter
 pares = list(filter(lambda x: x % 2 == 0, lista))
-
-# 4c) Usar reduce
-# Suma de [1..10]
 suma_total = reduce(lambda x, y: x + y, lista)
-
-# Producto de [1..5]
 producto = reduce(lambda x, y: x * y, [1, 2, 3, 4, 5])
-
-
-# Las Pruebas de ejecución
 
 if __name__ == "__main__":
     print("Suma(5, 3):", suma(5, 3))
@@ -52,3 +35,4 @@ if __name__ == "__main__":
     print("Pares:", pares)
     print("Suma [1..10]:", suma_total)
     print("Producto [1..5]:", producto)
+
