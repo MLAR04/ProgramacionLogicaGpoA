f41660dedaf23ebd8e119c51863f0bc9447ee2e9
HAlexis888 <al22760572@ite.edu.mx>
2025-09-12 14:48:50 -0700

    Create suma.py

---
+++ practica3/suma.py
@@ -0,0 +1,6 @@
+#Escribe una función suma(a,b) que solo dependa de sus parámetros.
+def suma(a, b):
+    "Funcion que recibe dos argumentos 'a' y 'b' t suma sus valores"
+    return a + b
+
+print(suma(5.5, 10))
