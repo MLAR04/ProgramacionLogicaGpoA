6eebdceefcee17698fef774ac2d84c429e8fd9a0
HAlexis888 <al22760572@ite.edu.mx>
2025-09-30 00:49:09 -0700

    Add files via upload

---
+++ Ejercicio5/baseConocimiento.py
@@ -0,0 +1,51 @@
+#Base de conocimiento (Diagnostico de enfermedades)
+from reglas import *
+import sys
+
+enfermo = {
+    'gripe':['dolor de cabeza', 'tos'],
+    'covid': ['perdida de olfato', 'cansancio', 'tos', 'fiebre'],
+    'migraña': ['nauseas', 'dolor de cabeza'],
+    'resfriado': ['congestion nasal', 'tos', 'fiebre']
+}
+sintomas_seleccionados = []
+
+a = 'fiebre'
+b = 'tos'
+c = 'dolor de cabeza'
+d = 'congestion nasal'
+e = 'nauseas'
+f = 'cansancio'
+g = 'perdida de olfato'
+
+while True:
+    sintomas = input(f'¿Qué síntomas tienes?\n a. {a}\n b. {b}\n c. {c}\n d. {d}\n e. {e}\n f. {f}\n g. {g}\n x. Otros\n s. Salir\n').lower()
+    if sintomas == "s":
+        break
+    elif sintomas == "x":
+        print("Si tienes sintomas que no sean los de las opciones no te puedo ayudar")
+        sys.exit()
+    elif sintomas in ['a', 'b', 'c', 'd', 'e', 'f', 'g']:
+        print("Algun otro sintoma?")
+        opciones = {'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f, 'g': g}
+        sintomas_seleccionados.append(opciones[sintomas])
+    else:
+        print("Ingresa una opcion valida")
+
+print("Regla 1.")
+te_sientes_mal()
+print("-----------------")
+print("Regla 2.")
+estas_enfermo(sintomas_seleccionados)
+print("-----------------")
+#Si la fiebre es mayor a 37.5 se agrega fiebre a sintomas seleccionados
+print("Regla 3.")
+fiebre_alta(sintomas_seleccionados)
+print("-----------------")
+print("Regla 4, 5, 6, 7.")
+enfermedades = ['gripe', 'covid', 'migraña', 'resfriado']
+for enfermedad in enfermedades:
+    if diagnosticar_enfermedad_especifica(enfermo, sintomas_seleccionados, enfermedad):
+        break
+else:
+    print("Tienes una enfermedad que no puedo diagnosticar")
---
+++ Ejercicio5/reglas.py
@@ -0,0 +1,47 @@
+#Funciones utilizadas para nuestra base de conocimiento
+import sys
+def te_sientes_mal():
+    "Si te sientes mal --> estas enfermo"
+    while True:
+        x = input("Te sientes mal? Si/No \n").lower()
+        if x == 'si':
+            print("Estas enfermo")
+            break
+        elif x == 'no':
+            print("Que haces aqui no estas enfremo")
+            break
+        else:
+            print("Ingresa un valor valido Si/No")
+
+
+def estas_enfermo(x):
+    "Si estas enfermo --> tienes sintomas"
+    if x == []:
+        print("No estas enfermo")
+    else:
+        print("Estas enfermo")
+
+def fiebre_alta(x):
+    "Si tienes temperatura alta --> tienes fiebre"
+    temperatura = int(input("Cual es tu temperatura?"))
+    if temperatura > 37.5:
+        if 'fiebre' in x:
+            print("Tienes fiebre alta")
+        else:
+            x.append('fiebre')
+    else:
+        print("No tienes fiebre")
+
+def diagnosticar_enfermedad_especifica(enfermo, sintomas_usuario, nombre):
+    if nombre in enfermo:
+        sintomas = enfermo[nombre]
+        if set(sintomas) == set(sintomas_usuario):
+            print(f'Tienes: {nombre}')
+            return True
+        elif sintomas_usuario == []:
+            print("No tienes ningún síntoma chic@")
+            sys.exit()
+    return False
+    
+
+
